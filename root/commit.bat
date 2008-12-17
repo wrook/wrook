2795b11790080ba3451013c4b46a4bf07cc98cad
Mathieu Sylvain <admin@wrook.org>
2008-12-17 09:15:18 -0500

    Added a batch file used for quick commits and added a better translation of a label

--- root/commit.bat
+++ root/commit.bat
@@ -1,10 +1,14 @@
-rem @echo off
+@echo off
 echo Starting the packing process
 call pack.bat
 echo Deleting compiled python files
 del *.pyc /s
 echo Adding all changes to git
 call git add . -A
+echo on
+call git status
+pause
+@echo off
 set /p desc=Enter a commit description: 
 echo Commiting: %1
 call git commit -m "%desc%"
